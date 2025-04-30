78c18427be68468134b5b053b3a383b934c6f053
howeniko <howeniko@cn-j001.server.mila.quebec>
2025-04-29 21:23:08 -0400

    Format

--- finetune_model.py
+++ finetune_model.py
@@ -35,10 +35,8 @@ def load_jsonl(path):
     with open(path) as f:
         return [json.loads(line) for line in f]
 
-
 def get_label_list(data):
     return sorted(list(set(ex['label'] for ex in data)))
-
 
 print('Loading datasets...')
 train_data = load_jsonl(TRAIN_FILE)
@@ -59,7 +57,6 @@ datasets = DatasetDict({'train': train_ds, 'validation': dev_ds})
 # 3. Tokenize
 def preprocess(example):
     return tokenizer(example['text'], truncation=True, padding=False)
-
 
 datasets = datasets.map(preprocess, batched=True)
 
